2a81a7bf80c31e6dc42cdd3011ecf47c015ea258
mason chen <646460780@qq.com>
2019-12-02 10:48:43 +0800

    Try2LdData

---
+++ log/train.py
@@ -0,0 +1,124 @@
+import argparse
+import math
+from datetime import datetime
+import h5py
+import numpy as np
+import tensorflow as tf
+import socket
+import importlib
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = BASE_DIR
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(ROOT_DIR, 'models'))
+sys.path.append(os.path.join(ROOT_DIR, 'utils'))
+import provider
+import tf_util
+import modelnet_dataset
+import modelnet_h5_dataset
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
+parser.add_argument('--model', default='pointnet2_cls_ssg', help='Model name [default: pointnet2_cls_ssg]')
+parser.add_argument('--log_dir', default='log', help='Log dir [default: log]')
+parser.add_argument('--num_point', type=int, default=1024, help='Point Number [default: 1024]')
+parser.add_argument('--max_epoch', type=int, default=251, help='Epoch to run [default: 251]')
+parser.add_argument('--batch_size', type=int, default=16, help='Batch Size during training [default: 16]')
+parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
+parser.add_argument('--momentum', type=float, default=0.9, help='Initial learning rate [default: 0.9]')
+parser.add_argument('--optimizer', default='adam', help='adam or momentum [default: adam]')
+parser.add_argument('--decay_step', type=int, default=200000, help='Decay step for lr decay [default: 200000]')
+parser.add_argument('--decay_rate', type=float, default=0.7, help='Decay rate for lr decay [default: 0.7]')
+parser.add_argument('--normal', action='store_true', help='Whether to use normal information')
+FLAGS = parser.parse_args()
+
+EPOCH_CNT = 0
+
+BATCH_SIZE = FLAGS.batch_size
+NUM_POINT = FLAGS.num_point
+MAX_EPOCH = FLAGS.max_epoch
+BASE_LEARNING_RATE = FLAGS.learning_rate
+GPU_INDEX = FLAGS.gpu
+MOMENTUM = FLAGS.momentum
+OPTIMIZER = FLAGS.optimizer
+DECAY_STEP = FLAGS.decay_step
+DECAY_RATE = FLAGS.decay_rate
+
+#MODEL = importlib.import_module(FLAGS.model) # import network module
+#MODEL_FILE = os.path.join(ROOT_DIR, 'models', FLAGS.model+'.py')
+LOG_DIR = FLAGS.log_dir
+if not os.path.exists(LOG_DIR): os.mkdir(LOG_DIR)
+#os.system('cp %s %s' % (MODEL_FILE, LOG_DIR)) # bkp of model def
+os.system('cp train.py %s' % (LOG_DIR)) # bkp of train procedure
+LOG_FOUT = open(os.path.join(LOG_DIR, 'log_train.txt'), 'w')
+LOG_FOUT.write(str(FLAGS)+'\n')
+
+# Shapenet official train/test split
+if FLAGS.normal:
+    assert(NUM_POINT<=10000)
+    DATA_PATH = os.path.join(ROOT_DIR, 'data/modelnet40_normal_resampled')
+    TRAIN_DATASET = modelnet_dataset.ModelNetDataset(root=DATA_PATH, npoints=NUM_POINT, split='train', normal_channel=FLAGS.normal, batch_size=BATCH_SIZE)
+    TEST_DATASET = modelnet_dataset.ModelNetDataset(root=DATA_PATH, npoints=NUM_POINT, split='test', normal_channel=FLAGS.normal, batch_size=BATCH_SIZE)
+else:
+    assert(NUM_POINT<=2048)
+    TRAIN_DATASET = modelnet_h5_dataset.ModelNetH5Dataset(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/train_files.txt'), batch_size=BATCH_SIZE, npoints=NUM_POINT, shuffle=True)
+    TEST_DATASET = modelnet_h5_dataset.ModelNetH5Dataset(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/test_files.txt'), batch_size=BATCH_SIZE, npoints=NUM_POINT, shuffle=False)
+
+def log_string(out_str):
+    LOG_FOUT.write(out_str+'\n')
+    LOG_FOUT.flush()
+    print(out_str)
+
+
+def train():
+    for epoch in range(MAX_EPOCH):
+        train_one_epoch()
+
+
+def train_one_epoch():
+    """ ops: dict mapping from string to tf ops """
+    is_training = True
+    
+    log_string(str(datetime.now()))
+
+    # Make sure batch data is of same size
+    cur_batch_data = np.zeros((BATCH_SIZE,NUM_POINT,TRAIN_DATASET.num_channel()))
+    cur_batch_label = np.zeros((BATCH_SIZE), dtype=np.int32)
+
+    total_correct = 0
+    total_seen = 0
+    loss_sum = 0
+    batch_idx = 0
+    while TRAIN_DATASET.has_next_batch():
+        batch_data, batch_label = TRAIN_DATASET.next_batch(augment=True)
+        #batch_data = provider.random_point_dropout(batch_data)
+        bsize = batch_data.shape[0]
+        cur_batch_data[0:bsize,...] = batch_data
+        cur_batch_label[0:bsize] = batch_label
+
+
+
+        total_seen += bsize
+
+        if (batch_idx+1)%50 == 0:
+            log_string(' ---- batch: %03d ----' % (batch_idx+1))
+
+            total_correct = 0
+            total_seen = 0
+            loss_sum = 0
+        batch_idx += 1
+
+    TRAIN_DATASET.reset()
+
+if __name__ == "__main__":
+    log_string('pid: %s'%(str(os.getpid())))
+    train()
+    LOG_FOUT.close()
+
+
+
+
+
+
+
